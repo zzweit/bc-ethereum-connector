3ebe78bc2392747daabc7fb99c3123a092cd81b6
Issac Liu <issac.liu.work@gmail.com>
2018-11-22 17:27:04 +0800

    added Delete function to supply chain smart contract

--- src/main/java/com/connector/blockchain/smartcontracts/SupplyChain.sol
+++ src/main/java/com/connector/blockchain/smartcontracts/SupplyChain.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.23;
 
 contract SupplyChain {
 
-  event BatchUpdated(uint indexed _batchId, uint _dateCreated, address _from, string state);
+  event BatchUpdated(uint indexed _batchId, uint _dateUpdated, address _from, string state);
+  event BatchRemoved(uint indexed _batchId, uint _dateRemoved, address _from);
 
   struct Batch {
     uint batchId;
@@ -22,7 +23,6 @@ contract SupplyChain {
     Batch memory batch;
     uint time = now;
     batch = Batch(_batchId, _quantity, _description, time, _sourceBatchIds);
-    
     batchIdToBatch[_batchId] = batch;
     batchIdExist[_batchId] = true; 
     string memory state = string(abi.encodePacked("Batch ", _batchId, " has been created at ", time));
@@ -44,5 +44,12 @@ contract SupplyChain {
     require(batchIdExist[_batchId] == true);
     Batch memory batch = batchIdToBatch[_batchId];
     return (batch.batchId, batch.description, batch.quantity, batch.dateCreated, batch.sourceBatchIds);
+  }
+
+  function deleteBatch(uint _batchId) external {
+    require(batchIdExist[_batchId] == true);
+    delete(batchIdToBatch[_batchId]);
+    delete(batchIdExist[_batchId]);
+    emit BatchRemoved(_batchId, now, msg.sender);
   }
 }
